35da3ad27cfda6637fe48ce508a1fc625e6346e8
Aleksandr Baikálov <asederado@gmail.com>
2022-07-25 17:13:32 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDxqS-2MmrVRbhmg49Bx2P1b4f_lFeSkQ0",
+  authDomain: "ounn-ea631.firebaseapp.com",
+  databaseURL: "https://ounn-ea631-default-rtdb.firebaseio.com",
+  projectId: "ounn-ea631",
+  storageBucket: "ounn-ea631.appspot.com",
+  messagingSenderId: "111990065034",
+  appId: "1:111990065034:web:1b1a4a2fd6d9aef5db6a73",
+  measurementId: "G-YL3CYJJN58"
+};
+
+
+const Web3StorageToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGE4MjU5MDcyYkUxRmEwODhhZTA3MTI3OTg2NDVEY0VBRjFGNDMzREQiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NTgyMjcyNTE1MTgsIm5hbWUiOiJvbm4ifQ.6AAz_KHuowFfJcxlf0jHbRoVEAKMv6iER_WSYkaUtr0";
+
+
+export default { firebaseConfig, Web3StorageToken };
